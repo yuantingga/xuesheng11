0b04cb120043ac066bbbf619491f5298b3a53bd5
YUAN.ING <1598538014@qq.com>
2022-01-21 21:32:07 +0800

    注册提交

--- js/登陆.js
+++ js/登陆.js
@@ -1,7 +1,4 @@
 $(function() {
-    console.log('我是dianji节点继续操作');
-    console.log(121212);
-    console.log("我是点击的内容");
-    console.log("我是zhuce分支");
-    console.log("-----------------------");
+    console.log("我是zhuche节点");
+    console.log("-----------");
 })
